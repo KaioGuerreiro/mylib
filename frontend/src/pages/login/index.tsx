1dbc1e5bc977b775e9ec74db0b96f89ecdfbfd36
Kaio Guerreiro <guerreirokaio@gmail.com>
2025-05-12 21:13:17 -0400

    feat: pagina de login

---
+++ frontend/src/pages/login/index.tsx
@@ -0,0 +1,37 @@
+export default function Login() {
+  return (
+    <div className="flex flex-col items-center justify-center w-screen h-screen bg-neutral-950">
+      <div className="flex flex-col items-center justify-center w-1/3 h-2/3 bg-neutral-200 rounded-lg shadow-lg">
+        <img src="../../public/mylibLogo.png" alt="logo" className="w-100 p-10" />
+        <h2 className="text-3xl font-bold mb-10 text-neutral-800">Login</h2>
+        <form action="" method="post" className="flex flex-col w-100">
+          <input
+            type="email"
+            placeholder="Digite seu email"
+            className="mb-4 p-2 border border-gray-300 rounded bg-neutral-50"
+          />
+          <input
+            type="password"
+            placeholder="Digite sua senha"
+            className="mb-4 p-2 border border-gray-300 rounded bg-neutral-50"
+          />
+          <button className="bg-neutral-900 text-neutral-50 p-2 rounded hover:bg-neutral-500 transition">
+            Login
+          </button>
+        </form>
+
+        <p className="mt-4 text-gray-400">
+          Ainda não tem uma conta?{" "}
+          <a href="/register" className="text-neutral-900 hover:underline">
+            Registre-se
+          </a>
+        </p>
+        <p className="mt-4 text-gray-400">
+          <a href="/forgot-password" className="text-neutral-900 hover:underline">
+            Esqueceu a senha?
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+}
